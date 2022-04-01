100fe7406718cae7677e2a53cb06d7f6ee02bd6d
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-04-01 09:58:02 +0200

    TxOut tapret commitment implementation

--- dbc/src/tapret/txout.rs
+++ dbc/src/tapret/txout.rs
@@ -12,3 +12,28 @@
 // You should have received a copy of the Apache 2.0 License
 // along with this software.
 // If not, see <https://opensource.org/licenses/Apache-2.0>.
+
+use bitcoin::TxOut;
+use commit_verify::embed_commit::ConvolveCommitVerify;
+use commit_verify::multi_commit::MultiCommitment;
+
+use super::{Lnpbp6, TapretProof, TapretTreeError};
+
+impl ConvolveCommitVerify<MultiCommitment, TapretProof, Lnpbp6> for TxOut {
+    type Commitment = TxOut;
+    type CommitError = TapretTreeError;
+
+    fn convolve_commit(
+        &self,
+        supplement: &TapretProof,
+        msg: &MultiCommitment,
+    ) -> Result<Self::Commitment, Self::CommitError> {
+        Ok(TxOut {
+            value: self.value,
+            script_pubkey: self
+                .script_pubkey
+                .convolve_commit(supplement, msg)?
+                .into_inner(),
+        })
+    }
+}
