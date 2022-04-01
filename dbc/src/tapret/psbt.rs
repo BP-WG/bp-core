2ee7f50a77c50e6703f4db404ff7c34936e1536b
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-04-01 09:58:43 +0200

    Psbt

--- dbc/src/tapret/psbt.rs
+++ dbc/src/tapret/psbt.rs
@@ -12,3 +12,49 @@
 // You should have received a copy of the Apache 2.0 License
 // along with this software.
 // If not, see <https://opensource.org/licenses/Apache-2.0>.
+
+use bitcoin::Transaction;
+use commit_verify::multi_commit::MultiCommitment;
+use commit_verify::{EmbedCommitProof, EmbedCommitVerify};
+use psbt::Psbt;
+
+use super::{Lnpbp6, TapretProof, TapretPsbtError};
+
+impl EmbedCommitProof<MultiCommitment, (Psbt, Transaction), Lnpbp6>
+    for TapretProof
+{
+    fn restore_original_container(
+        &self,
+        commit_container: &(Psbt, Transaction),
+    ) -> Result<(Psbt, Transaction), TapretPsbtError> {
+        let (psbt, tx) = commit_container.clone();
+
+        for (output, txout) in (psbt.outputs, tx.output) {
+            if txout.script_pubkey.is_v1_p2tr() {
+                return self.restore_original_container(&(output, txout));
+            }
+        }
+
+        return Err(TapretPsbtError::NoTaprootOutput);
+    }
+}
+
+impl EmbedCommitVerify<MultiCommitment, Lnpbp6> for (Psbt, Transaction) {
+    type Proof = TapretProof;
+    type CommitError = TapretPsbtError;
+
+    fn embed_commit(
+        &mut self,
+        msg: &MultiCommitment,
+    ) -> Result<Self::Proof, Self::CommitError> {
+        let (psbt, tx) = self;
+
+        for (output, txout) in (&mut psbt.outputs, &mut tx.output) {
+            if txout.script_pubkey.is_v1_p2tr() {
+                return (output, txout).embed_commit(msg);
+            }
+        }
+
+        return Err(TapretPsbtError::NoTaprootOutput);
+    }
+}
