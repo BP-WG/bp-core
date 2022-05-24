22c53e4753645ef256dbc27f99a5d746e843fec6
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-05-24 13:30:52 +0200

    TxoSeal trait

--- seals/src/txout/mod.rs
+++ seals/src/txout/mod.rs
@@ -19,47 +19,10 @@
 pub mod blind;
 mod error;
 mod explicit;
-
-use std::str::FromStr;
+// mod proto;
+mod seal;
 
 pub use error::{MethodParseError, VerifyError, WitnessVoutError};
 pub use explicit::ExplicitSeal;
-
-/// Method of single-use-seal closing.
-#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Display)]
-#[cfg_attr(
-    feature = "serde",
-    derive(Serialize, Deserialize),
-    serde(crate = "serde_crate")
-)]
-#[derive(StrictEncode, StrictDecode)]
-#[strict_encoding(by_value)]
-#[repr(u8)]
-#[non_exhaustive]
-pub enum CloseMethod {
-    /// Seal is closed over the message in form of OP_RETURN commitment present
-    /// in the first OP_RETURN-containing transaction output.
-    #[display("opret1st")]
-    OpretFirst = 0x00,
-
-    /// Seal is closed over the message in form of Taproot-based OP_RETURN
-    /// commitment present in the first Taproot transaction output.
-    #[display("tapret1st")]
-    TapretFirst = 0x01,
-}
-
-impl FromStr for CloseMethod {
-    type Err = MethodParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s.to_lowercase() {
-            s if s == CloseMethod::OpretFirst.to_string() => {
-                CloseMethod::OpretFirst
-            }
-            s if s == CloseMethod::TapretFirst.to_string() => {
-                CloseMethod::TapretFirst
-            }
-            _ => return Err(MethodParseError(s.to_owned())),
-        })
-    }
-}
+// pub use proto::TxoProtocol;
+pub use seal::{CloseMethod, TxoSeal};
