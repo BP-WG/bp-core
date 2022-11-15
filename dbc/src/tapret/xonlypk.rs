929bc0e8b6bca3d456a70200c496e44f3949f910
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-15 21:48:05 +0100

    Unit tests for tapret xonlypk commitments

--- dbc/src/tapret/xonlypk.rs
+++ dbc/src/tapret/xonlypk.rs
@@ -78,7 +78,11 @@ impl ConvolveCommitVerify<lnpbp4::CommitmentHash, TapretProof, Lnpbp6>
             TreeNode::with_tap_script(script_commitment, 0)
         };
 
-        // TODO: Check with <https://github.com/rust-bitcoin/rust-bitcoin/issues/1393>
+        // rust-bitcoin API has this inefficiency: while `TapLeafHash` can be
+        // a valid merkle root (for script trees with a single leaf), it is not
+        // accepted by the tap_tweak API.
+        //
+        // Details: <https://github.com/rust-bitcoin/rust-bitcoin/issues/1393>
         let merkle_root =
             TapBranchHash::from_inner(root.node_hash().into_inner());
         // TODO: Use secp instance from Lnpbp6
@@ -91,5 +95,81 @@ impl ConvolveCommitVerify<lnpbp4::CommitmentHash, TapretProof, Lnpbp6>
         };
 
         Ok((output_key, proof))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use amplify::Wrapper;
+    use bitcoin::hashes::Hash;
+    use bitcoin_scripts::LeafScript;
+    use commit_verify::lnpbp4::CommitmentHash;
+    use secp256k1::XOnlyPublicKey;
+
+    use super::*;
+
+    #[test]
+    fn key_path() {
+        let internal_key = XOnlyPublicKey::from_str(
+            "c5f93479093e2b8f724a79844cc10928dd44e9a390b539843fb83fbf842723f3",
+        )
+        .unwrap();
+        let msg = CommitmentHash::from_inner(Hash::hash(b""));
+        let path_proof = TapretPathProof::new();
+
+        let (outer_key, proof) =
+            internal_key.convolve_commit(&path_proof, &msg).unwrap();
+
+        let script_commitment = TapScript::commit(&(msg, 0));
+        let root = TreeNode::with_tap_script(script_commitment, 0);
+        let merkle_root =
+            TapBranchHash::from_inner(root.node_hash().into_inner());
+        let (real_key, _) =
+            internal_key.tap_tweak(SECP256K1, Some(merkle_root));
+
+        assert_eq!(outer_key, real_key);
+
+        assert_eq!(proof, TapretProof {
+            path_proof,
+            internal_key
+        });
+
+        assert!(ConvolveCommitProof::<
+            CommitmentHash,
+            UntweakedPublicKey,
+            Lnpbp6,
+        >::verify(&proof, &msg, outer_key)
+        .unwrap());
+    }
+
+    #[test]
+    fn single_script() {
+        let internal_key = XOnlyPublicKey::from_str(
+            "c5f93479093e2b8f724a79844cc10928dd44e9a390b539843fb83fbf842723f3",
+        )
+        .unwrap();
+        let msg = CommitmentHash::from_inner(Hash::hash(b""));
+        let path_proof = TapretPathProof::with(
+            TapretNodePartner::RightLeaf(LeafScript::tapscript(default!())),
+            88,
+        )
+        .unwrap();
+
+        let (outer_key, proof) =
+            internal_key.convolve_commit(&path_proof, &msg).unwrap();
+
+        assert_eq!(proof, TapretProof {
+            path_proof,
+            internal_key
+        });
+
+        assert!(ConvolveCommitProof::<
+            CommitmentHash,
+            UntweakedPublicKey,
+            Lnpbp6,
+        >::verify(&proof, &msg, outer_key)
+        .unwrap());
     }
 }
