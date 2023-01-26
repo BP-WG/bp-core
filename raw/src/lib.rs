d9d88b10941903b929552b12757d1f35abd2d427
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-01-26 20:58:00 +0100

    Improve raw::VarIntBytes

--- raw/src/lib.rs
+++ raw/src/lib.rs
@@ -20,12 +20,44 @@ mod serialize;
 mod sha256;
 mod tx;
 
-use amplify::confinement::Confined;
 pub use sha256::Sha256;
 pub use tx::{
     LockTime, Sats, ScriptPubkey, SeqNo, SigScript, Tx, TxIn, TxOut, TxVer,
     Txid,
 };
+pub use types::{VarIntArray, VarIntBytes};
 
-pub type VarIntArray<T> = Confined<Vec<T>, 0, { u64::MAX as usize }>;
-pub type VarIntBytes = VarIntArray<u8>;
+mod types {
+    use std::fmt::{Formatter, LowerHex, UpperHex};
+
+    use amplify::confinement::Confined;
+    use amplify::hex::ToHex;
+
+    pub type VarIntArray<T> = Confined<Vec<T>, 0, { u64::MAX as usize }>;
+
+    #[derive(
+        Wrapper, WrapperMut, Clone, Ord, PartialOrd, Eq, PartialEq, Hash,
+        Default, Debug, From
+    )]
+    #[wrapper(RangeOps, BorrowSlice)]
+    #[wrapper_mut(RangeMut, BorrowSliceMut)]
+    pub struct VarIntBytes(VarIntArray<u8>);
+
+    impl From<Vec<u8>> for VarIntBytes {
+        fn from(value: Vec<u8>) -> Self {
+            Self(Confined::try_from(value).expect("u64 >= usize"))
+        }
+    }
+
+    impl LowerHex for VarIntBytes {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            f.write_str(&self.0.as_inner().to_hex())
+        }
+    }
+
+    impl UpperHex for VarIntBytes {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            f.write_str(&self.0.as_inner().to_hex().to_uppercase())
+        }
+    }
+}
