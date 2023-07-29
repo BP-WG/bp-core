e2cfd6356915108566ed548ee571f32707497312
Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-07-29 17:14:32 +0200

    primitives: add P2PKH and P2SH handling methods to ScriptPubkey

--- primitives/src/script.rs
+++ primitives/src/script.rs
@@ -63,6 +63,46 @@ pub enum OpCode {
     /// Push the array `0x01` onto the stack.
     #[display("OP_PUSHNUM_1")]
     PushNum1 = OP_PUSHNUM_1,
+
+    /// Duplicates the top stack item.
+    #[display("OP_DUP")]
+    Dup = OP_DUP,
+
+    /// Pushes 1 if the inputs are exactly equal, 0 otherwise.
+    #[display("OP_EQUAL")]
+    Equal = OP_EQUAL,
+
+    /// Returns success if the inputs are exactly equal, failure otherwise.
+    #[display("OP_EQUALVERIFY")]
+    EqualVerify = OP_EQUALVERIFY,
+
+    /// Pop the top stack item and push its RIPEMD160 hash.
+    #[display("OP_RIPEMD160")]
+    Ripemd160 = OP_RIPEMD160,
+
+    /// Pop the top stack item and push its SHA1 hash.
+    #[display("OP_SHA1")]
+    Sha1 = OP_SHA1,
+
+    /// Pop the top stack item and push its SHA256 hash.
+    #[display("OP_SHA256")]
+    Sha256 = OP_SHA256,
+
+    /// Pop the top stack item and push its RIPEMD(SHA256) hash.
+    #[display("OP_HASH160")]
+    Hash160 = OP_HASH160,
+
+    /// Pop the top stack item and push its SHA256(SHA256) hash.
+    #[display("OP_HASH256")]
+    Hash256 = OP_HASH256,
+
+    /// <https://en.bitcoin.it/wiki/OP_CHECKSIG> pushing 1/0 for success/failure.
+    #[display("OP_CHECKSIG")]
+    CheckSig = OP_CHECKSIG,
+
+    /// <https://en.bitcoin.it/wiki/OP_CHECKSIG> returning success/failure.
+    #[display("OP_CHECKSIGVERIFY")]
+    CheckSigVerify = OP_CHECKSIGVERIFY,
 }
 
 #[derive(Wrapper, WrapperMut, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, From, Default)]
@@ -104,11 +144,49 @@ impl ScriptPubkey {
         Self(ScriptBytes::from(Confined::with_capacity(capacity)))
     }
 
+    pub fn p2pkh(hash: impl Into<[u8; 20]>) -> Self {
+        let mut script = Self::with_capacity(25);
+        script.push_opcode(OpCode::Dup);
+        script.push_opcode(OpCode::Hash160);
+        script.push_slice(&hash.into());
+        script.push_opcode(OpCode::EqualVerify);
+        script.push_opcode(OpCode::CheckSig);
+        script
+    }
+
+    pub fn p2sh(hash: impl Into<[u8; 20]>) -> Self {
+        let mut script = Self::with_capacity(23);
+        script.push_opcode(OpCode::Hash160);
+        script.push_slice(&hash.into());
+        script.push_opcode(OpCode::Equal);
+        script
+    }
+
     pub fn op_return(data: &[u8]) -> Self {
         let mut script = Self::with_capacity(ScriptBytes::len_for_slice(data.len()) + 1);
         script.push_opcode(OpCode::Return);
-        script.0.push_slice(data);
+        script.push_slice(data);
         script
+    }
+
+    /// Checks whether a script pubkey is a P2PKH output.
+    #[inline]
+    pub fn is_p2pkh(&self) -> bool {
+        self.0.len() == 25 &&
+            self.0[0] == OP_DUP &&
+            self.0[1] == OP_HASH160 &&
+            self.0[2] == OP_PUSHBYTES_20 &&
+            self.0[23] == OP_EQUALVERIFY &&
+            self.0[24] == OP_CHECKSIG
+    }
+
+    /// Checks whether a script pubkey is a P2SH output.
+    #[inline]
+    pub fn is_p2sh(&self) -> bool {
+        self.0.len() == 23 &&
+            self.0[0] == OP_HASH160 &&
+            self.0[1] == OP_PUSHBYTES_20 &&
+            self.0[22] == OP_EQUAL
     }
 
     pub fn is_op_return(&self) -> bool { self[0] == OpCode::Return as u8 }
