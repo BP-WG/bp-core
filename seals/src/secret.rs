f116b5f7cbf29e4879d427ee397df66791940462
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-12-29 19:59:18 +0100

    seals: return Display and FromStr impl for SecretSeal

--- seals/src/secret.rs
+++ seals/src/secret.rs
@@ -19,7 +19,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use amplify::Bytes32;
+use std::fmt::{self, Display, Formatter};
+use std::str::FromStr;
+
+use amplify::{Bytes32, Wrapper};
+use baid58::{Baid58ParseError, Chunking, FromBaid58, ToBaid58, CHUNKING_32CHECKSUM};
 
 /// Confidential version of transaction outpoint-based single-use-seal
 #[derive(Wrapper, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, From)]
@@ -38,3 +42,42 @@ pub struct SecretSeal(
     #[from([u8; 32])]
     Bytes32,
 );
+
+impl ToBaid58<32> for SecretSeal {
+    const HRI: &'static str = "utxob";
+    const CHUNKING: Option<Chunking> = CHUNKING_32CHECKSUM;
+    fn to_baid58_payload(&self) -> [u8; 32] { self.0.into_inner() }
+    fn to_baid58_string(&self) -> String { self.to_string() }
+}
+impl FromBaid58<32> for SecretSeal {}
+impl FromStr for SecretSeal {
+    type Err = Baid58ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        SecretSeal::from_baid58_maybe_chunked_str(s, ':', ' ')
+    }
+}
+impl Display for SecretSeal {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "{::^}", self.to_baid58())
+        } else {
+            write!(f, "{::^.3}", self.to_baid58())
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn secret_seal_baid58() {
+        let baid58 = "utxob:2eFrirU-RjqLnqR74-AKRfdnc9M-DpvSRjmZG-mFPrw7nvu-Te1wy83";
+        let seal: SecretSeal = baid58.parse().unwrap();
+        assert_eq!(baid58, seal.to_string());
+        assert_eq!(baid58.replace('-', ""), format!("{seal:#}"));
+        assert_eq!(seal.to_string(), seal.to_baid58_string());
+        let reconstructed = SecretSeal::from_str(&baid58.replace('-', "")).unwrap();
+        assert_eq!(reconstructed, seal);
+    }
+}
