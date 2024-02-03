06fced918b6c7e26b14eac9a8ff017dccd911066
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2024-02-03 22:54:45 +0100

    stl: add Vesper support

--- src/bin/bpcore-stl.rs
+++ src/bin/bpcore-stl.rs
@@ -19,8 +19,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fs;
+
+use bc::stl::{bp_consensus_stl, bp_tx_stl};
+use bp::stl::bp_core_stl;
+use commit_verify::stl::commit_verify_stl;
 use strict_encoding::libname;
-use strict_types::parse_args;
+use strict_types::stl::std_stl;
+use strict_types::{parse_args, SystemBuilder};
 
 fn main() {
     let (format, dir) = parse_args();
@@ -41,7 +47,7 @@ fn main() {
     )
     .expect("unable to write to the file");
 
-    bc::stl::bp_consensus_stl()
+    bp_consensus_stl()
         .serialize(
             format,
             dir.as_ref(),
@@ -56,10 +62,10 @@ fn main() {
         )
         .expect("unable to write to the file");
 
-    bp::stl::bp_core_stl()
+    bp_core_stl()
         .serialize(
             format,
-            dir,
+            dir.as_ref(),
             "0.1.0",
             Some(
                 "
@@ -70,4 +76,39 @@ fn main() {
             ),
         )
         .expect("unable to write to the file");
+
+    let std = std_stl();
+    let tx = bp_tx_stl();
+    let bp = bp_core_stl();
+    let cv = commit_verify_stl();
+
+    let sys = SystemBuilder::new()
+        .import(bp)
+        .unwrap()
+        .import(tx)
+        .unwrap()
+        .import(cv)
+        .unwrap()
+        .import(std)
+        .unwrap()
+        .finalize()
+        .expect("not all libraries present");
+
+    let dir = dir.unwrap_or_else(|| ".".to_owned());
+
+    let tt = sys.type_tree("BPCore.AnchorMerkleTreeTapretProof").unwrap();
+    fs::write(format!("{dir}/Anchor.MerkleTree.Tapret.vesper",), format!("{tt}")).unwrap();
+
+    let tt = sys.type_tree("BPCore.AnchorMerkleTreeOpretProof").unwrap();
+    fs::write(format!("{dir}/Anchor.MerkleTree.Opret.vesper",), format!("{tt}")).unwrap();
+
+    let tt = sys
+        .type_tree("BPCore.AnchorMerkleBlockTapretProof")
+        .unwrap();
+    fs::write(format!("{dir}/Anchor.MerkleBlock.Tapret.vesper",), format!("{tt}")).unwrap();
+
+    let tt = sys
+        .type_tree("BPCore.AnchorMerkleProofTapretProof")
+        .unwrap();
+    fs::write(format!("{dir}/Anchor.MerkleProof.Tapret.vesper",), format!("{tt}")).unwrap();
 }
